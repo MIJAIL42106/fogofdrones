9bd82513e4d70bd4d51e16cb62ad4c5fd9a496f9
Mijail <mijail42106@gmail.com>
2026-02-17 20:35:28 -0300

    Proyecto base

---
+++ src/main/java/grupo2/fod/fogofdrones/FogofdronesApplication.java
@@ -0,0 +1,13 @@
+package grupo2.fod.fogofdrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FogofdronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FogofdronesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo2/fod/fogofdrones/FogofdronesApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.fod.fogofdrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FogofdronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
